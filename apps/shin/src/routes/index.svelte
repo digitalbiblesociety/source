cb96f6c99dd8c720c59dbbac1a5c6c2989ec2de2
Jon <jon@dbs.org>
2022-08-13 17:38:32 -0500

    Front page defaults to languages index

--- apps/shin/src/routes/index.svelte
+++ apps/shin/src/routes/index.svelte
@@ -1,54 +1,63 @@
 <script context="module">
 	// See https://kit.svelte.dev/docs#loading
 	export async function load({fetch}) {
+
+		const iso_excluded = import.meta.env.VITE_ISO_EXCLUDED.split(',');
+		const iso_included = import.meta.env.VITE_ISO_INCLUDED;
+		const current_country = import.meta.env.VITE_COUNTRY_SITE;
+
 		const baseUrl = import.meta.env.VITE_BASE_API
-		const res = await fetch(`${baseUrl}/bibles_dbs.json`)
-		if (res.ok) {
-			return {
-				props: {
-					bibles: await res.json(),
-				},
+		const response = await fetch(`${baseUrl}/countries/${current_country}.json`);
+		let country = await response.json();
+
+		const bible_request = await fetch(`${baseUrl}/bibles_dbs.json`);
+		let bibles = await bible_request.json();
+		bibles = bibles.map((bible) => bible.iso);
+		let languages = [];
+
+		country.languages.forEach((language) => {
+			if (bibles.includes(language.iso)) {
+				let lang = {
+					'tt': language.name,
+					'tv': language.autonym,
+					'id': language.iso,
+					'po': language.population,
+					'ci': language.country_id,
+					'cn': country.name,
+					'bc': bibles.filter((iso) => iso == language.iso).length,
+					'rc': '',
+					'fc': '',
+				}
+				if (iso_included) {
+					if (iso_included.split(',').includes(language.iso)) {
+						languages.push(lang);
+					}
+				} else {
+					if (!iso_excluded.includes(language.iso)) {
+						languages.push(lang);
+					}
+				}
 			}
-		}
-	
+		});
+
+		console.log(languages)
+
 		return {
-			status: res.status,
-			error: new Error('Could not load bibles JSON'),
+			props: {
+				languages: languages,
+			},
 		}
 	}
 </script>
 <script>
-	import {Datatable} from '@dbs/svelte-datatables'
-	import { page } from '$app/stores'
-	import {t} from '$lib/translations'
+	import { onMount } from "svelte"
+	import { browser } from '$app/env'
+	import { Datatable } from "@dbs/svelte-datatables"
 	import filters from './bibles/filters.js'
-	import MX from "$lib/map/MX.svelte"
-	import IN from "$lib/map/IN.svelte"
 
-    const options = {
-		MX:	MX,
-		IN:	IN
-	}
-	export let bibles
-
-	bibles = bibles.filter(function (bible) {
-    	return bible.ci === import.meta.env.VITE_COUNTRY_SITE;
-	});
-
-	// Filter
-	let currentLanguage = $page.url.searchParams.get('language')
-	if(currentLanguage) {
-		bibles = bibles.filter(function (bible) {
-			let languages = filters[import.meta.env.VITE_COUNTRY_SITE][0].options;
-			for (let index = 0; index < languages.length; index++) {
-				const language = languages[index];
-				if(currentLanguage == language.label) {
-					return language.value.split('|').includes(bible.iso)
-				}	
-			}
-			return false
-		});
-	}
+	export let locale = 'en'
+	export let languages
+	export let translations
 
 	const settings = {
 		labels: {
@@ -58,65 +67,68 @@
 			previous: "<",
 			next: ">",
 		},
-		customFilters: !currentLanguage ? filters[import.meta.env.VITE_COUNTRY_SITE] : []
+		customFilters: filters[import.meta.env.VITE_COUNTRY_SITE]
 	}
+	
+	let js = false
+	onMount(async () => {
+		js = true
+	})
+
+	const table_row = (row, locale) =>  `
+	<tr class="${row.bc + row.fc + row.rc > 0 ? '' : 'opacity-40'}">
+		<td class="whitespace-nowrap px-6 py-4 text-sm font-medium">
+			<a href="/languages/${row.id}">
+				<div class="text-sm text-gray-900 dark:text-gray-300">
+					${row.tt}
+				</div>
+				<div class="text-xs text-gray-500">${row.tv}</div>
+			</a>
+		</td>
+		<td class="hidden whitespace-nowrap text-sm sm:table-cell">
+			${row.id}
+		</td>
+		<td>${row.po ? row.po.toLocaleString(locale) : ""}</td>
+	</tr>`
 
 	let rows
 </script>
 
+{#if languages}
+<div class="mx-auto w-4/5 pt-8">
 
+	{#if browser}
+		<Datatable classList="relative" data="{languages}" bind:dataRows="{rows}" settings={settings}>
+			<thead class="bg-gray-50 text-gray-600">
+				<th data-key="(row) => row.tt + ' ' + row.iso" class="sortable">{translations?.thead?.title ?? 'title'}</th>
+				<th data-name="language" data-key="id" class="sortable hidden sm:table-cell">iso</th>
+				<th data-key="po" class="sortable">{translations?.thead?.population ?? 'population'}</th>
+			</thead>
+			<tbody>
+				{#if rows}
+					{#each $rows as row}
+						{@html table_row(row)}
+					{/each}
+				{/if}
+			</tbody>
+		</Datatable>
+	{/if}
 
-<div class="relative mx-auto py-2 px-3 md:w-full">
-	<a href="/map" class="text-green-500 absolute top-2 right-2 hover:text-green-800 rounded-full">
-		<svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
-			<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z" clip-rule="evenodd" />
-		 </svg>
-	</a>
-
-	<Datatable
-		classList="relative"
-		data="{bibles}"
-		bind:dataRows="{rows}"
-		settings={settings}>
-		<thead>
-			<th data-name="id" data-key="id" class="hidden">id</th>
-			<th data-name="file_set" data-key="fs" class="hidden">file_set</th>
-			<th data-name="language" data-key="(row) => row.ln + ' ' + row.iso" class="sortable">{$t('common.language')}</th>
-			<th data-name="title" data-key="(row) => row.tt + ' ' + row.tv" class="sortable"> {$t('common.title')}
-			</th>
-			<th data-name="country_id" data-key="ci" class="hidden">country id</th>
-			<th data-name="date" data-key="dt" class="sortable hidden md:table-cell">{$t('common.date')}</th>
-		</thead>
-		<tbody>
-			{#if rows}
-				{#each $rows as row}
-					<tr class="h-12">
-						<td class="dark:white hidden whitespace-nowrap px-6 py-4 text-stone-900">{row.id}</td>
-						<td class="whitespace-nowrap px-6 py-4 text-stone-900">
-							<div class="text-sm text-stone-900 dark:text-white">{row.ln}</div>
-							<div class="text-xs italic text-stone-500 dark:text-stone-400">{row.iso}</div>
-						</td>
-						<td class="px-6 py-4">
-							<a
-								href="/bibles/{row.id}"
-								class="text-primary-700 hover:underline dark:text-white">
-								<div class="max-w-sm overflow-hidden text-ellipsis text-sm">{row.tt}</div>
-								<div
-									class="max-w-sm overflow-hidden text-ellipsis text-sm text-stone-600 dark:text-stone-400">
-									{row.tv ?? ''}
-								</div>
-							</a>
-						</td>
-						<td class="hidden whitespace-nowrap px-6 py-4 text-stone-900">
-							{row.ci}
-						</td>
-						<td
-							class="hidden whitespace-nowrap px-6 py-4 text-stone-900 md:table-cell">
-							{row.dt}
-						</td>
-					</tr>
+	<noscript>
+		<table class="mx-auto w-4/5 pt-8">
+			<thead class="bg-gray-50 text-gray-600">
+				<th class="sortable">{translations?.thead?.title ?? 'title'}</th>
+				<th class="sortable"></th>
+				<th class="sortable hidden sm:table-cell">iso</th>
+			</thead>
+			<tbody>
+				{#each languages as row}
+					{#if row.bc + row.fc + row.rc > 0}
+						{@html table_row(row)}
+					{/if}
 				{/each}
-			{/if}
-		</tbody>
-	</Datatable>
+			</tbody>
+			</table>
+	</noscript>
 </div>
+{/if}
